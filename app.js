71410d3482fb223b9f903be52dbcf1a4d05c152d
aish1499 <aishwaryabpotadar@gmail.com>
2020-01-31 21:51:47 +0530

    first commit

--- app.js
+++ app.js
@@ -1,91 +1,64 @@
-// console.log(1);
-// console.log(1);
-// setTimeout(()=>{
-//     console.log("function called");
-
-// },2000);
-// console.log(3);
-// console.log(4);
-
-
-// const getTodos = resource => {
-//     return new Promise(resolve,reject =>{
-// const request = new XMLHttpRequest();
-// request.open = ("GET",resource);
-// //request.open("GET", "http://jsonplaceholder.typicode.com/todos/");
-// request.send();
-
-// request.addEventListener('readystatechange', e=>{
-//     //console.log(request.readystate);
-//    if (request.readystate===4 && request.status===200)
-//    {
-//        resolve(request.responseText);
-//     console.log("data ready!");
-//    }else if(request.readystate===4 ){
-//     console.log("data not found");
-//    }
-//   });
-//  });
-// getTodos("http://jsonplaceholder.typicode.com/todos/1");
-
-// }
-
-const CityForm = document.querySelector('form');
-const card = document.querySelector('card');
-const detail = document.querySelector('detail');
+const cityForm = document.querySelector('form')
+const card = document.querySelector('.card')
+const time = document.querySelector('.time')
 const icon = document.querySelector('.icon img')
-const time = document.querySelector('time');
-const updateGUI = details =>{
-    const {cityDetail,weather} = data;
-    console.log("citydetail");
-    console.log("weather");
-  details.innerHTML=`
-<h3 class="my-3">${cityDetail.EnglishName}</h3>
-<div class="my-3">${weather.Weathertext}</div>
+const details = document.querySelector('.details')
 
-<span>${currentTime.getHours()}:${currentTime.getMinutes()}</span>
-<div class="display-4 my-3">
-<span>${weather.Temperature.Metric.value}</span>
-<span>&deg;C</span>
-</div>
-</div>`
-
-};
-let  timeSrc = '';
-if(weather.IsDayTime){
-    timeSrc ="'images/day.svg'"
-}
-else{
-    timeSrc="'images/day.svg'";
-}
-
-
-time.setAttribute('src',timeSrc);
-iconSrc = `icons/${weather.WeatherIcon}.svg`
-icon.setAttribute('src', iconSrc)
-
-
-
-const updateCity= async (data) =>{
+const updateGUI = (data) => {
+    // const city = data.cityDetails
+    // const weather = data.weather
+    let {cityDetails, weather} = data;
+    console.log(cityDetails);
+    console.log(weather);
+    const currentTime = new Date(weather.EpochTime)
     
-    const cityDetail = await getCity(city);
-    const weather = await getWeather(cityDetail.key);
 
-    return{
-        cityDetail: cityDetail,
-        weather:weather
-    };
-};
- 
- 
-CityForm.addEventListener("submit", e => {
-    e.preventDefault();
-    const city = CityForm.city.value.trim();
-    updateCity(city)
-    .then(data => updateGUI(data))
-    .catch(err => console.log(err));
-
-    if(card.classList.contains("d-none")){
-        card.classList.remove("d-none")
+    details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <span>${currentTime.getHours()}:${currentTime.getMinutes()}</span>
+    <div class="display-4 my-3">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+        
+    </div>
+    `   
+    let timeSrc = ''
+    if(weather.IsDayTime){
+        timeSrc = 'images/day.svg'
+    } else {
+        timeSrc = 'images/night.svg'
     }
-});
+
+    time.setAttribute('src', timeSrc);
+
+    iconSrc = `images/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute('src', iconSrc)
+}
+
+const updateCity = async (city) => {
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key)
+
+    return {
+        cityDetails,
+        weather
+    }
+}
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    const city = cityForm.city.value.trim();
+    
+    updateCity(city)
+        .then(data => updateGUI(data))
+        .catch(err => console.log(err));
+    cityForm.reset()
+    // console.log(card.classList)
+    //Making the card appear
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+    }
+
+})
--- forcast.js
+++ forcast.js
@@ -1,25 +1,29 @@
-const key = '63hO68tgR00LmNLF7vNIcYpzCUgWNVKg';
+const key = 'mRm9WTNGMmL3PZ1IhZ7Yz1fUFsa8mpXG'
 
-const getWeather = async id =>{
-    const base ="http://dataservice.accuweather.com/currentconditions/v1/";
-    const query = `${id}?apikey=${key}`;
+//Get weather informations:
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    query = `${id}?apikey=${key}`;
+
     const response = await fetch(base + query);
-    const data =await response.json();
-    return data;
+    const data = await response.json();
+    return data[0]
 }
 
-const getCity = async city =>{
-    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+
+//Get city informations:
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
-    const response= await fetch(base + query);
-    const data =await response.json();
-    return data[0];
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0]
+}
 
-};
-// getWeather("213490")
+// getCity('bangalore')
+//     .then(data => getWeather(data.Key))  //Get promise from getweather
 //     .then(data => console.log(data))
-//     .catch(err => console.log(err));
-//getCity("rome")
+//     .catch(err => console.log(err))
 
-//getCity('banglore').then(data=> console.log(data)).then (err=>console.log(err));
+// getWeather('349727');
